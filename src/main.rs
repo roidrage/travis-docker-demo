a0074999ebe26c724dd83c8ae56f02a2a761dbe5
Joe Corcoran <joe@corcoran.io>
2017-05-18 21:20:10 +0200

    Use cfg

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@ fn main() {
     println!("{}", check());
 }
 
+#[cfg(target_arch = "x86_64")]
 fn check() -> bool {
     std::mem::size_of::<usize>() == 8
+}
+
+#[cfg(target_arch = "i686")]
+fn check() -> bool {
+    std::mem::size_of::<usize>() == 4
 }
 
 #[test]
